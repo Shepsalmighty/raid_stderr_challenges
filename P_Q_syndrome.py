7e975a641673086908b29770e5e55ca4a1fa1456
sheps <sheps@sheps.com>
2024-11-26 21:18:08 +0000

    beat the challenge

--- P_Q_syndrome.py
+++ P_Q_syndrome.py
@@ -79,9 +79,16 @@ def divide(var1, var2):
             return n
 
 
-def Q_syndrome_pt2(block1, block2, block3, block4, block5, block6):
+def P_syndrome_pt2(block):
     recovered_data = []
-    for a, b, c, d, e, f in zip(block1, block2, block3, block4, block5, block6):
+    for a, b, c, d, e, f in zip(block[0], block[1], block[2], block[3], block[4], block[5]):
+        recovered_data.append((int(a) ^ int(b) ^ int(c) ^ int(d) ^ int(e) ^ int(f)))
+    return bytes(recovered_data)
+
+
+def Q_syndrome_pt2(block):
+    recovered_data = []
+    for a, b, c, d, e, f in zip(block[0], block[1], block[2], block[3], block[4], block[5]):
         a = multiply(1, a)
         b = multiply(2, b)
         c = multiply(3, c)
@@ -145,152 +152,167 @@ def recover_all_pt1(block1, block2, P_block, Q_block):
                 print("PANIC!!!!")
         return recovered_block1, recovered_block2
 
-def recover_all_pt2(block1, block2, block3, block4, block5 ,block6, P_block, Q_block):
+
+
+
+def recover_all_pt2(block, P_block, Q_block):
+
+    missing_data = []  # list of indexes in block where data is missing
+    for d in range(len(block)):
+        if len(block[d]) == 0:
+            missing_data.append(d)
+
     #returns all blocks if they have data
-    if len(block1) > 0 and\
-            len(block2) > 0 and\
-            len(block3) > 0 and\
-            len(block4) > 0 and\
-            len(block5) > 0 and\
-            len(block6) > 0:
-        return block1, block2, block3, block4, block5, block6
+    if len(block[0]) > 0 and\
+            len(block[1]) > 0 and\
+            len(block[2]) > 0 and\
+            len(block[3]) > 0 and\
+            len(block[4]) > 0 and\
+            len(block[5]) > 0:
+        return block[0], block[1], block[2], block[3], block[4], block[5]
 
     # recovers data from 1 lost drive using P syndrome (XOR)
-    #case 1, block1 empty but remaining blocks and P_block have data
+    #case 1, block[0 empty but remaining blocks and P_block have data
     if len(P_block) > 0:
-        if len(block1) == 0 and\
-            len(block2) > 0 and\
-            len(block3) > 0 and\
-            len(block4) > 0 and\
-            len(block5) > 0 and\
-            len(block6) > 0:
+        if len(block[0]) == 0 and\
+            len(block[1]) > 0 and\
+            len(block[2]) > 0 and\
+            len(block[3]) > 0 and\
+            len(block[4]) > 0 and\
+            len(block[5]) > 0:
             recovered_data = []
-            for a, b, c, d, e, f in zip(P_block, block2, block3, block4, block5, block6):
+            for a, b, c, d, e, f in zip(P_block, block[1], block[2], block[3], block[4], block[5]):
                 recovered_data.append(int(a) ^ int(b) ^ int(c) ^ int(d) ^ int(e) ^ int(f))
-            block1 = bytes(recovered_data)
-            return block1, block2, block3, block4, block5, block6
+            block[0] = bytes(recovered_data)
+            return block[0], block[1], block[2], block[3], block[4], block[5]
 
-        # case 2, block2 empty but remaining blocks and P_block have data
-        elif len(block2) == 0 and\
-            len(block1) > 0 and\
-            len(block3) > 0 and\
-            len(block4) > 0 and\
-            len(block5) > 0 and\
-            len(block6) > 0:
+        # case 2, block[1 empty but remaining blocks and P_block have data
+        elif len(block[1]) == 0 and\
+            len(block[0]) > 0 and\
+            len(block[2]) > 0 and\
+            len(block[3]) > 0 and\
+            len(block[4]) > 0 and\
+            len(block[5]) > 0:
             recovered_data = []
-            for a, b, c, d, e, f in zip(P_block, block1, block3, block4, block5, block6):
+            for a, b, c, d, e, f in zip(P_block, block[0], block[2], block[3], block[4], block[5]):
                 recovered_data.append(int(a) ^ int(b) ^ int(c) ^ int(d) ^ int(e) ^ int(f))
-            block2 = bytes(recovered_data)
-            return block1, block2, block3, block4, block5, block6
+            block[1] = bytes(recovered_data)
+            return block[0], block[1], block[2], block[3], block[4], block[5]
 
-            # case 3, block3 empty but remaining blocks and P_block have data
-        elif len(block3) == 0 and \
-             len(block1) > 0 and \
-             len(block2) > 0 and \
-             len(block4) > 0 and \
-             len(block5) > 0 and \
-             len(block6) > 0:
+            # case 3, block[2 empty but remaining blocks and P_block have data
+        elif len(block[2]) == 0 and \
+             len(block[0]) > 0 and \
+             len(block[1]) > 0 and \
+             len(block[3]) > 0 and \
+             len(block[4]) > 0 and \
+             len(block[5]) > 0:
             recovered_data = []
-            for a, b, c, d, e, f in zip(P_block, block1, block2, block4, block5, block6):
+            for a, b, c, d, e, f in zip(P_block, block[0], block[1], block[3], block[4], block[5]):
                 recovered_data.append(int(a) ^ int(b) ^ int(c) ^ int(d) ^ int(e) ^ int(f))
-            block3 = bytes(recovered_data)
-            return block1, block2, block3, block4, block5, block6
+            block[2] = bytes(recovered_data)
+            return block[0], block[1], block[2], block[3], block[4], block[5]
 
-            # case 4, block4 empty but remaining blocks and P_block have data
-        elif len(block4) == 0 and \
-                len(block1) > 0 and \
-                len(block2) > 0 and \
-                len(block3) > 0 and \
-                len(block5) > 0 and \
-                len(block6) > 0:
+            # case 4, block[3 empty but remaining blocks and P_block have data
+        elif len(block[3]) == 0 and \
+                len(block[0]) > 0 and \
+                len(block[1]) > 0 and \
+                len(block[2]) > 0 and \
+                len(block[4]) > 0 and \
+                len(block[5]) > 0:
             recovered_data = []
-            for a, b, c, d, e, f in zip(P_block, block1, block2, block3, block5, block6):
+            for a, b, c, d, e, f in zip(P_block, block[0], block[1], block[2], block[4], block[5]):
                 recovered_data.append(int(a) ^ int(b) ^ int(c) ^ int(d) ^ int(e) ^ int(f))
-            block4 = bytes(recovered_data)
-            return block1, block2, block3, block4, block5, block6
+            block[3] = bytes(recovered_data)
+            return block[0], block[1], block[2], block[3], block[4], block[5]
 
-            # case 5, block5 empty but remaining blocks and P_block have data
-        elif len(block5) == 0 and \
-             len(block1) > 0 and \
-             len(block2) > 0 and \
-             len(block3) > 0 and \
-             len(block4) > 0 and \
-             len(block6) > 0:
+            # case 5, block[4 empty but remaining blocks and P_block have data
+        elif len(block[4]) == 0 and \
+             len(block[0]) > 0 and \
+             len(block[1]) > 0 and \
+             len(block[2]) > 0 and \
+             len(block[3]) > 0 and \
+             len(block[5]) > 0:
             recovered_data = []
-            for a, b, c, d, e, f in zip(P_block, block1, block2, block3, block4, block6):
+            for a, b, c, d, e, f in zip(P_block, block[0], block[1], block[2], block[3], block[5]):
                 recovered_data.append(int(a) ^ int(b) ^ int(c) ^ int(d) ^ int(e) ^ int(f))
-            block5 = bytes(recovered_data)
-            return block1, block2, block3, block4, block5, block6
+            block[4] = bytes(recovered_data)
+            return block[0], block[1], block[2], block[3], block[4], block[5]
 
-            # case 6, block6 empty but remaining blocks and P_block have data
-        elif len(block6) == 0 and \
-                len(block1) > 0 and \
-                len(block2) > 0 and \
-                len(block3) > 0 and \
-                len(block4) > 0 and \
-                len(block5) > 0:
+            # case 6, block[5 empty but remaining blocks and P_block have data
+        elif len(block[5]) == 0 and \
+                len(block[0]) > 0 and \
+                len(block[1]) > 0 and \
+                len(block[2]) > 0 and \
+                len(block[3]) > 0 and \
+                len(block[4]) > 0:
             recovered_data = []
-            for a, b, c, d, e, f in zip(P_block, block1, block2, block3, block4, block5):
+            for a, b, c, d, e, f in zip(P_block, block[0], block[1], block[2], block[3], block[4]):
                 recovered_data.append(int(a) ^ int(b) ^ int(c) ^ int(d) ^ int(e) ^ int(f))
-            block6 = bytes(recovered_data)
-            return block1, block2, block3, block4, block5, block6
+            block[5] = bytes(recovered_data)
+            return block[0], block[1], block[2], block[3], block[4], block[5]
 
 
     #Q_recovery if missing 1 data block and the P block
-    #1st case block1 and P_block missing
-    if len(block1) == 0 and len(P_block) == 0:
-        not_really_Q = Q_syndrome_pt2([0 for _ in range(len(Q_block))],block2, block3, block4, block5, block6)
+    #1st case block[0 and P_block missing
+    if len(block[0]) == 0 and len(P_block) == 0:
+        block[0] = [0 for _ in range(len(Q_block))]
+        not_really_Q = Q_syndrome_pt2(block)
         recovered_data = []
         for a, b in zip(not_really_Q, Q_block):
             recovered_data.append(divide((a ^ b), 1))
-        return bytes(recovered_data), block2, block3, block4, block5, block6
+        return bytes(recovered_data), block[1], block[2], block[3], block[4], block[5]
         # recovered_data = []
-        # for a, b, c, d, e, f in zip(P_block, block1, block2, block3, block4, block5):
+        # for a, b, c, d, e, f in zip(P_block, block[0, block[1, block[2, block[3, block[4):
         #     recovered_data.append(int(a) ^ (2* int(b) ^(3*int(c)) ^(4*int(d)) ^(5*int(e)) ^(6*(int(f)))))
-        # for a, b, c, d, e, f in zip(Q_block, block2, block3, block4, block5, block6):
+        # for a, b, c, d, e, f in zip(Q_block, block[1, block[2, block[3, block[4, block[5):
 
-        # return bytes(recovered_data), block2, block3, block4, block5, block6
-    #2nd case block2 and P_block missing
-    if len(block2) == 0 and len(P_block) == 0:
-        not_really_Q = Q_syndrome_pt2(block1, [0 for _ in range(len(Q_block))], block3, block4, block5, block6)
+        # return bytes(recovered_data), block[1, block[2, block[3, block[4, block[5
+    #2nd case block[1 and P_block missing
+    if len(block[1]) == 0 and len(P_block) == 0:
+        block[1] = [0 for _ in range(len(Q_block))]
+        not_really_Q = Q_syndrome_pt2(block)
         recovered_data = []
         for a, b in zip(not_really_Q, Q_block):
             recovered_data.append(divide((a ^ b), 2))
-        return block1, bytes(recovered_data), block3, block4, block5, block6
+        return block[0], bytes(recovered_data), block[2], block[3], block[4], block[5]
 
-    # 3rd case block3 and P_block missing
-    if len(block3) == 0 and len(P_block) == 0:
-        not_really_Q = Q_syndrome_pt2(block1, block2, [0 for _ in range(len(Q_block))], block4, block5, block6)
+    # 3rd case block[2 and P_block missing
+    if len(block[2]) == 0 and len(P_block) == 0:
+        block[2] = [0 for _ in range(len(Q_block))]
+        not_really_Q = Q_syndrome_pt2(block)
         recovered_data = []
         for a, b in zip(not_really_Q, Q_block):
             recovered_data.append(divide((a ^ b), 3))
-        return block1, block2, bytes(recovered_data), block4, block5, block6
+        return block[0], block[1], bytes(recovered_data), block[3], block[4], block[5]
 
-    # 4th case block4 and P_block missing
-    if len(block4) == 0 and len(P_block) == 0:
-        not_really_Q = Q_syndrome_pt2(block1, block2, block3, [0 for _ in range(len(Q_block))], block5, block6)
+    # 4th case block[3 and P_block missing
+    if len(block[3]) == 0 and len(P_block) == 0:
+        block[3] = [0 for _ in range(len(Q_block))]
+        not_really_Q = Q_syndrome_pt2(block)
         recovered_data = []
         for a, b in zip(not_really_Q, Q_block):
             recovered_data.append(divide((a ^ b), 4))
-        return block1, block2, block3, bytes(recovered_data), block5, block6
+        return block[0], block[1], block[2], bytes(recovered_data), block[4], block[5]
 
-    # 5th case block5 and P_block missing
-    if len(block5) == 0 and len(P_block) == 0:
-        not_really_Q = Q_syndrome_pt2(block1, block2, block3, block4, [0 for _ in range(len(Q_block))], block6)
+    # 5th case block[4 and P_block missing
+    if len(block[4]) == 0 and len(P_block) == 0:
+        block[4] = [0 for _ in range(len(Q_block))]
+        not_really_Q = Q_syndrome_pt2(block)
         recovered_data = []
         for a, b in zip(not_really_Q, Q_block):
             recovered_data.append(divide((a ^ b), 5))
-        return block1, block2, block3, block4, bytes(recovered_data), block6
+        return block[0], block[1], block[2], block[3], bytes(recovered_data), block[5]
 
-    # 6th case block6 and P_block missing
-    if len(block6) == 0 and len(P_block) == 0:
-        not_really_Q = Q_syndrome_pt2(block1, block2, block3, block4, block5, [0 for _ in range(len(Q_block))])
+    # 6th case block[5 and P_block missing
+    if len(block[5]) == 0 and len(P_block) == 0:
+        block[5] = [0 for _ in range(len(Q_block))]
+        not_really_Q = Q_syndrome_pt2(block)
         recovered_data = []
         for a, b in zip(not_really_Q, Q_block):
             recovered_data.append(divide((a ^ b), 6))
-        return block1, block2, block3, block4, block5 , bytes(recovered_data)
+        return block[0], block[1], block[2], block[3], block[4] , bytes(recovered_data)
 
-    # for a,b,c,d,e,f in zip(block1, block2, block3, block4, block5, block6):
+    # for a,b,c,d,e,f in zip(block1, block2, block[2, block4, block5, block[5):
 
     #TODO: make this function not fucking suck
 
@@ -298,34 +320,71 @@ def recover_all_pt2(block1, block2, block3, block4, block5 ,block6, P_block, Q_b
 
     #if missing both data blocks, we must Q recover D2 (block2) first, and then we can P recover D1 (block1)
     #see also Reed Solomon algorithm https://anadoxin.org/blog/error-recovery-in-raid6.html/
-    if len(block1) == 0 and len(block2) == 0:
+    # if len(block1) == 0 and len(block2) == 0:
+    #     recovered_block2 = bytearray()
+    #     recovered_block1 = bytearray()
+    #     for a, b in zip(P_block, Q_block):
+    #         for n in range(0, 256):
+    #             n_xor_2n = Q_syndrome(bytes((n,)), bytes((n,)))
+    #             if n_xor_2n[0] == a ^ b:
+    #                 recovered_block2.append(n)
+    #                 recovered_block1.append(a ^ n)
+    #                 break
+    #         else:
+    #             print("PANIC!!!!")
+    #     return recovered_block1, recovered_block2
+
+    #TODO: delete this
+    # missing_data = []  # list of indexes in block where data is missing
+    # for d in range(len(block)):
+    #     if len(block[d]) == 0:
+    #         missing_data.append(d)
+
+    if len(missing_data) == 2:
+
+        block[missing_data[0]] = [0 for _ in range(len(Q_block))]
+        block[missing_data[1]] = [0 for _ in range(len(Q_block))]
+
+        not_P = P_syndrome_pt2(block)  # not_P is P ^ block[missing[0]] ^ block[missing[1]]
+        not_Q = Q_syndrome_pt2(block)  # not_Q is Q if the missing data were 0s
+
         recovered_block2 = bytearray()
         recovered_block1 = bytearray()
-        for a, b in zip(P_block, Q_block):
+
+
+        for a, b, c, d in zip(P_block, not_P, Q_block, not_Q,):
             for n in range(0, 256):
-                n_xor_2n = Q_syndrome(bytes((n,)), bytes((n,)))
-                if n_xor_2n[0] == a ^ b:
-                    recovered_block2.append(n)
-                    recovered_block1.append(a ^ n)
+                # a ^ b = the xor of our 2 missing data blocks
+                m = a ^ b ^ n
+
+                recovered_n = multiply(missing_data[0]+1, n)
+                recovered_m = multiply(missing_data[1]+1, m)
+
+                if c ^ d == recovered_m ^ recovered_n:
+                    recovered_block2.append(m)
+                    recovered_block1.append(n)
                     break
             else:
                 print("PANIC!!!!")
-        return recovered_block1, recovered_block2
+        block[missing_data[0]] = recovered_block1
+        block[missing_data[1]] = recovered_block2
 
-def number_missing_drives(block1, block2, block3, block4, block5 ,block6):
+        return block
+
+def number_missing_drives(block):
     stripe_count = 0
     count = 0
-    if len(block1) == 0:
+    if len(block[0]) == 0:
         count += 1
-    if len(block2) == 0:
+    if len(block[1]) == 0:
         count += 1
-    if len(block3) == 0:
+    if len(block[2]) == 0:
         count += 1
-    if len(block4) == 0:
+    if len(block[3]) == 0:
         count += 1
-    if len(block5) == 0:
+    if len(block[4]) == 0:
         count += 1
-    if len(block6) == 0:
+    if len(block[5]) == 0:
         count += 1
 
     return count
--- RAID6_pt2.py
+++ RAID6_pt2.py
@@ -5,12 +5,12 @@ from P_Q_syndrome import recover_all_pt2
 from P_Q_syndrome import number_missing_drives
 
 def read_file(file_name):
-    if not file_name:
+    try:
+        with open(file_name, "rb") as file:
+            content = file.read()
+            return content
+    except FileNotFoundError as e:
         return []
-
-    with open(file_name, "rb") as file:
-        content = file.read()
-        return content
 
 
 def read_raid6():
@@ -42,110 +42,43 @@ def read_raid6():
             #       open(paths[7], "rb") as sdh_file,):
 
             # blocksize = 512
+            #TODO: probably read_file(Path(paths[0])) etc
+            sd = []
+            for num in range(8):
+                sd.append(read_file(paths[num]))
 
-            sda = read_file(paths[0])
-            sdb = read_file(paths[1])
-            sdc = read_file(paths[2])
-            sdd = read_file(paths[3])
-            sde = read_file(paths[4])
-            sdf = read_file(paths[5])
-            sdg = read_file(paths[6])
-            sdh = read_file(paths[7])
+            # ordered_blocks = sd[count % 8:] + sd[:count % 8]
 
                 #TODO: use arrays to shorten redundant code in the if/elif block
                 # files = [sda, sdb, sdc, sdd, sde, sdf, sdg, sdh]
                 # blocks = [None, None, None, None, None, None, None]
                 # to read the file bytes in the correct order we do (count % 8) such that the P and Q syndrome are known
 
-                # for P in range(8):
-                #     if P % count ==
-            if count % 8 == 0:
-                P_block = sdh
-                Q_block = sda
-                block1 = sdb
-                block2 = sdc
-                block3 = sdd
-                block4 = sde
-                block5 = sdf
-                block6 = sdg
+            P_index = 7 - (count%8)
+            Q_index = (P_index + 1) % 8
+            data1_index = (Q_index + 1) % 8
+            block = []
+            for d in range(6):
+                block.append(sd[(data1_index + d) % 8])
+            P_block = sd[P_index]
+            Q_block = sd[Q_index]
 
-            if count % 8 == 1:
-                P_block = sdg
-                Q_block = sdh
-                block1 = sda
-                block2 = sdb
-                block3 = sdc
-                block4 = sdd
-                block5 = sde
-                block6 = sdf
 
-            if count % 8 == 2:
-                P_block = sdf
-                Q_block = sdg
-                block1 = sdh
-                block2 = sda
-                block3 = sdb
-                block4 = sdc
-                block5 = sdd
-                block6 = sde
+            # P_block, Q_block, block1, block2, block3, block4, block5, block6 = ordered_blocks
 
-            if count % 8 == 3:
-                P_block = sde
-                Q_block = sdf
-                block1 = sdg
-                block2 = sdh
-                block3 = sda
-                block4 = sdb
-                block5 = sdc
-                block6 = sdd
-
-            if count % 8 == 4:
-                P_block = sdd
-                Q_block = sde
-                block1 = sdf
-                block2 = sdg
-                block3 = sdh
-                block4 = sda
-                block5 = sdb
-                block6 = sdc
-
-            if count % 8 == 5:
-                P_block = sdc
-                Q_block = sdd
-                block1 = sde
-                block2 = sdf
-                block3 = sdg
-                block4 = sdh
-                block5 = sda
-                block6 = sdb
-
-            if count % 8 == 6:
-                P_block = sdb
-                Q_block = sdc
-                block1 = sdd
-                block2 = sde
-                block3 = sdf
-                block4 = sdg
-                block5 = sdh
-                block6 = sda
-
-            if count % 8 == 7:
-                P_block = sda
-                Q_block = sdb
-                block1 = sdc
-                block2 = sdd
-                block3 = sde
-                block4 = sdf
-                block5 = sdg
-                block6 = sdh
-
-            if number_missing_drives(block1, block2, block3, block4, block5, block6) >= 2:
+            # if number_missing_drives(block1, block2, block3, block4, block5, block6) >= 2:
+            #     return count
+            if number_missing_drives(block) > 2:
                 return count
 
-            block1, block2, block3, block4, block5, block6 = recover_all_pt2(
-                block1, block2, block3, block4, block5, block6, P_block, Q_block)
+            block = recover_all_pt2(block, P_block, Q_block)
 
-            file.write(block1 + block2 + block3 + block4 + block5 + block6)
+            # block1, block2, block3, block4, block5, block6 = recover_all_pt2(
+            #     block1, block2, block3, block4, block5, block6, P_block, Q_block)
+
+            # file.write(block1 + block2 + block3 + block4 + block5 + block6)
+            for b in block:
+                file.write(b)
 
                 # print((block1 + block2).decode("utf-8"), end="")
 
